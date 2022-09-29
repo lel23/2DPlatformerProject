6c5acfef2628086e3ffe9a6f795a28942a96a47f
Leslie Le <lel23@mail.wlu.edu>
2022-09-29 23:14:58 +0200

    Player updates
    * Forgot to add one tile change
    
    Changes:
    * Removed unnecessary script
    * Created new player prefab
    * Updated jumping conditions
    * Refactoring with Door and Obstacle

--- Assets/Scripts/LesliePlayer.cs
+++ Assets/Scripts/LesliePlayer.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Player : MonoBehaviour
+public class LesliePlayer : MonoBehaviour
 {
-    public string level;
     public float blood = 100;
     private Vector3 startPos;
 
@@ -13,11 +12,13 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb2d;
     public float speed;
     public float jumpForce;
-    public bool grounded = false;
     public bool crouched = false;
 
     [Header("Grounding")]
+    public bool grounded = false;
     public LayerMask ground;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
 
     [Header("Animation")]
     private SpriteRenderer sr;
@@ -25,15 +26,12 @@ public class Player : MonoBehaviour
     public Sprite crouch;
     public Sprite jump;
 
-    public GameObject effect;
     public GameObject bloodSplash;
 
     [Header("Audio")]
     private AudioSource source;
     public AudioClip jumpSound;
-    public AudioClip flagGetSound;
 
-    // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
@@ -49,7 +47,7 @@ public class Player : MonoBehaviour
         Vector2 vel = rb2d.velocity;
         vel.x = Input.GetAxis("Horizontal") * speed;
 
-        UpdateGrounding();
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, ground);
 
         bool inputJump1 = Input.GetKeyDown(KeyCode.Space);
         bool inputJump2 = Input.GetKeyDown(KeyCode.UpArrow);
@@ -58,44 +56,24 @@ public class Player : MonoBehaviour
             vel.y = jumpForce;
         }
 
-        Debug.Log(grounded);
         rb2d.velocity = vel;
 
         // on death here we use uimanager to update the view on bloodcount
         if (Input.GetKeyDown(KeyCode.R))
         {
-            Instantiate(effect, transform.position, new Quaternion(90, 0, 0, 1));
-            Instantiate(bloodSplash, transform.position, Quaternion.identity);
-            blood -= 50;
-            transform.position = startPos;
-        } else if (Input.GetKeyDown(KeyCode.T)) {
+            //Instantiate(effect, transform.position, new Quaternion(90, 0, 0, 1));
+            OnDeath();
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    public void OnDeath()
     {
-        if (other.CompareTag("Flag"))
-        {
-            source.clip = flagGetSound;
-            source.Play();
-            SceneManager.LoadScene(level);
-        }
-    }
-
-    // I think I will update this method with a box collider rather
-    // than using ray casts
-    private void UpdateGrounding()
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 1.1f, ground);
-
-        Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
-
-        if (hit.collider != null)
-        {
-            grounded = true;
-        } else {
-            grounded = false;
-        }
+        Instantiate(bloodSplash, transform.position, Quaternion.identity);
+        blood -= 50;
+        transform.position = startPos;
     }
 }
