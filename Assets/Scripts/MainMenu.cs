6236049aeb7433953634ad7bb62b39551b29dd09
Minh Chill <chieuminh0507@gmail.com>
2022-10-05 19:54:41 +0200

    Import Fonts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,6 +7,11 @@ public class MainMenu: MonoBehaviour
 {
     public void PlayGame()
     {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ChooseLevel()
+    {
         SceneManager.LoadScene(1);
     }
 
