44e93ab81287ee48f90a4243e41c31b0b479bd7a
Leslie Le <lel23@mail.wlu.edu>
2022-10-05 19:56:12 +0200

    Added pause menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,21 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Canvas;
+    public GameObject Camera;
+    private AudioSource source;
+    public AudioClip splat;
+    private static bool paused = false;
+
+
     void Start()
     {
-
+        Canvas.gameObject.SetActive(false);
+        source = GetComponent<AudioSource>();
     }
-
+    
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Application.Quit();
+            if (paused)
+            {
+                Time.timeScale = 1f;
+                Canvas.gameObject.SetActive(false);
+                paused = false;
+            }
+            else
+            {
+                Time.timeScale = 0.0f;
+                Canvas.gameObject.SetActive(true);
+                paused = true;
+            }
         }
+            // Application.Quit();
+        }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        Canvas.gameObject.SetActive(false);
+        source.PlayOneShot(splat);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        source.PlayOneShot(splat);
+    }
+
+    public void LevelSelect()
+    {
+        SceneManager.LoadScene("LevelSelect");
+        source.PlayOneShot(splat);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        source.PlayOneShot(splat);
+        Application.Quit();
     }
 }
