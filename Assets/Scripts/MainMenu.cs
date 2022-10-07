55e74cc1d184e80c14a0b0a40e37e37bf1e47832
Minh Chill <chieuminh0507@gmail.com>
2022-10-07 02:54:50 +0200

    MainMenu + Level Selection Edit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,14 +7,28 @@ public class MainMenu: MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1); // change to name maybe
     }
 
-    public void ChooseLevel()
+    public void Level1()
     {
         SceneManager.LoadScene(1);
     }
 
+    public void Level2()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(4);
+    }
 
     public void QuitGame()
     {
