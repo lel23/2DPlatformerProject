cf5f223057b794a99562cbb232e2fe78d5b03d22
Minh Chill <chieuminh0507@gmail.com>
2022-10-07 19:38:42 +0200

    Menu Anim

--- Assets/Scripts/MenuScripts/MainMenu.cs
+++ Assets/Scripts/MenuScripts/MainMenu.cs
@@ -5,29 +5,49 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu: MonoBehaviour
 {
+
+    public GameObject Loader;
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1); // change to name maybe
     }
 
+    public void LoadScene(string sceneName)
+    {
+        targetScene = sceneName;
+        StartCoroutine(loadSceneRoutine());
+    }
+
+    private IEnumerator loadSceneRoutine()
+    {
+        Loader.SetActive(true);
+        AsyncOperation op = SceneManager.LoadSceneAsync(targetScene);
+
+        while (!op.isDone)
+            yield return null;
+
+        Loader.SetActive(false);
+    }
+
     public void Level1()
-    {
-        SceneManager.LoadScene(1);
-    }
-
-    public void Level2()
-    {
-        SceneManager.LoadScene(2);
-    }
-
-    public void Level3()
     {
         SceneManager.LoadScene(3);
     }
 
-    public void Level4()
+    public void Level2()
     {
         SceneManager.LoadScene(4);
+    }
+
+    public void Level3()
+    {
+        SceneManager.LoadScene(5);
+    }
+
+    public void Level4()
+    {
+        SceneManager.LoadScene(6);
     }
 
     public void QuitGame()
