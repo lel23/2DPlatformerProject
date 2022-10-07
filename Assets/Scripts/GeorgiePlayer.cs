6e1371de80a5a5905a21c467fe717a0335d5cc78
gstammer <cprebels333@comcast.net>
2022-10-07 10:18:47 +0200

    testing different movement

--- Assets/Scripts/GeorgiePlayer.cs
+++ Assets/Scripts/GeorgiePlayer.cs
@@ -2,109 +2,172 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GeorgiePlayer : MonoBehaviour
 {
-    // What is changed from Leslie's script:
-    // removed instantiation of her splatter effect
-    // changed grounding layer to Ground (in inspector)
-    // changed blood splat to my prefab (in inspector)
-
-    public string level;
-    public float blood = 100;
+    public static float livesLost;
     private Vector3 startPos;
+    private bool isDead;
 
-    [Header("Movement")]
+    // MOVEMMENT
     private Rigidbody2D rb2d;
     public float speed;
     public float jumpForce;
+
+    // GROUNDING
     public bool grounded = false;
-    public bool crouched = false;
-
-    [Header("Grounding")]
     public LayerMask ground;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius;
 
-    [Header("Animation")]
+    // ANIMATION
     private SpriteRenderer sr;
-    public Sprite side;
-    public Sprite crouch;
-    public Sprite jump;
+    public SpriteRenderer bloodSr;
 
-    //public GameObject effect;
+    public Sprite[] framesRightLeft;
+    public Sprite still;
+    public Sprite jumpFall;
+    public float framesPerSecond = 2;
+    int currentFrameIndexLeftRight = 0;
+    float frameTimer;
+
+    public Sprite[] framesBlood;
     public GameObject bloodSplash;
 
-    [Header("Audio")]
+    // AUDIO
     private AudioSource source;
     public AudioClip jumpSound;
-    public AudioClip flagGetSound;
 
-    // Start is called before the first frame update
     void Start()
     {
+        isDead = false;
         rb2d = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
         source = GetComponent<AudioSource>();
 
         startPos = transform.position;
+        livesLost = 0;
+
+        frameTimer = (1f / framesPerSecond);
+        currentFrameIndexLeftRight = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (PauseMenu.paused || isDead) return;
+
+        ///////////// MOVEMENT
         Vector2 vel = rb2d.velocity;
-        vel.x = Input.GetAxis("Horizontal") * speed;
 
-        UpdateGrounding();
+        // georgie: testing jumping with momentum
+        if (Input.GetKey(KeyCode.LeftArrow)) vel.x = (-1) * speed;
+        else if (Input.GetKey(KeyCode.RightArrow)) vel.x = speed;
+        else if (grounded) vel.x = 0;
 
-        bool inputJump1 = Input.GetKeyDown(KeyCode.Space);
-        bool inputJump2 = Input.GetKeyDown(KeyCode.UpArrow);
-        if ((inputJump1 || inputJump2) && grounded)
-        {
-            vel.y = jumpForce;
-        }
-
-        Debug.Log(grounded);
         rb2d.velocity = vel;
 
-        // on death here we use uimanager to update the view on bloodcount
-        if (Input.GetKeyDown(KeyCode.R))
+        bool inputJump = Input.GetKeyDown(KeyCode.UpArrow);
+        if (inputJump && grounded)
         {
-            //Instantiate(effect, transform.position, new Quaternion(90, 0, 0, 1));
-            Instantiate(bloodSplash, transform.position, Quaternion.identity);
-            blood -= 50;
-            transform.position = startPos;
+            //vel.y = jumpForce;
+            rb2d.AddForce(new Vector2(0, jumpForce));
+            source.clip = jumpSound;
+            source.Play();
         }
-        else if (Input.GetKeyDown(KeyCode.T))
+        //rb2d.velocity = vel;
+
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, ground);
+        // Debug.Log(grounded);
+
+        // kys
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            isDead = true;
+            StartCoroutine(OnDeath());
         }
+        // REMOVE WHEN OUT OF DEVELOPMENT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        else if (Input.GetKeyDown(KeyCode.T)) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        ///////////// ANIMATION
+        if (livesLost >= 0 && livesLost <= 5)
+            bloodSr.sprite = framesBlood[(int)livesLost];
+        if (rb2d.velocity.x == 0 && rb2d.velocity.y == 0)
+        {
+            sr.sprite = still;
+        }
+
+        if (rb2d.velocity.x < 0 || rb2d.velocity.x > 0) // Moving left or right
+        {
+            if (rb2d.velocity.x < 0) // Left
+                sr.flipX = true;
+            if (rb2d.velocity.x > 0) // Right
+                sr.flipX = false;
+
+            frameTimer -= Time.deltaTime;
+
+            if (frameTimer <= 0)
+            {
+                currentFrameIndexLeftRight++;
+                if (currentFrameIndexLeftRight == 5)
+                {
+                    currentFrameIndexLeftRight = 0;
+                }
+
+                frameTimer = (1f / framesPerSecond);
+                sr.sprite = framesRightLeft[currentFrameIndexLeftRight];
+            }
+        }
+
+        // jumping animation
+        if (!grounded) sr.sprite = jumpFall;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Obstacle")
+        {
+            if (!isDead)
+            {
+                isDead = true;
+                StartCoroutine(OnDeath());
+            }
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Flag"))
+        if (other.gameObject.CompareTag("Replenish"))
         {
-            source.clip = flagGetSound;
-            source.Play();
-            SceneManager.LoadScene(level);
+            if (livesLost > 0)
+            {
+                livesLost--;
+                Destroy(other.gameObject);
+            }
         }
     }
 
-    // I think I will update this method with a box collider rather
-    // than using ray casts
-    private void UpdateGrounding()
+    public IEnumerator OnDeath()
     {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector3.down, 1.1f, ground);
+        Instantiate(bloodSplash, transform.position, Quaternion.identity);
+        livesLost++;
 
-        Debug.DrawLine(transform.position, transform.position + Vector3.down * 1.1f, Color.red);
+        sr.enabled = false;
+        bloodSr.enabled = false;
+        yield return new WaitForSeconds(1);
 
-        if (hit.collider != null)
+        sr.enabled = true;
+        bloodSr.enabled = true;
+        transform.position = startPos;
+
+        isDead = false;
+        if (livesLost > 5)
         {
-            grounded = true;
-        }
-        else
-        {
-            grounded = false;
+            livesLost = 0;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
 }
